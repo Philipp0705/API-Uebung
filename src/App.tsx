a732525cb5e9abe102cf6fbf3d4a3cf5ed1e60bc
Philipp0705 <philipp.paessens@smartcondata.de>
2025-07-10 11:37:13 +0200

    filter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 
 interface daten {
-    userID: number;
+    userId: number;
     id: number;
     title: string;
     body: string;
@@ -14,15 +14,17 @@ export default function App() {
     const [title, setTitle] = useState("")
     const [body, setBody] = useState("")
 
+    const [filter, setFilter] = useState("")
+
     const [data, setData] = useState<daten[]>([])
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(daten => daten.json())
             .then(neueDaten => {
-                const datenarray = neueDaten.map((single: any) => ({ userID: single.userId, id: single.id, title: single.title, body: single.body }))
-                setData(datenarray)
+                const datenarray = filter === "" ? neueDaten : neueDaten.filter((item: any) => item.userId === Number(filter))
+                    setData(datenarray)
             })
-    }, [])
+    }, [filter])
 
     return (
         <div>
@@ -33,18 +35,21 @@ export default function App() {
                 <input type="text" placeholder="Body" value={body} onChange={(e) => setBody(e.target.value)} />
 
                 <button onClick={() => {
-                    const newData = {userID: Number(userID), id: Number(ID), title: title, body: body}
+                    const newData = { userID: Number(userID), id: Number(ID), title: title, body: body }
                     fetch('https://jsonplaceholder.typicode.com/posts', {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify(newData),
                     })
-                    .then(send => send.json())
-                    .then(log => {
-                        console.log(log)
-                        setData(prevData => [log, ...prevData])
-                    })
+                        .then(send => send.json())
+                        .then(log => {
+                            console.log(log)
+                            setData(prevData => [log, ...prevData])
+                        })
                 }}>Send Data to API</button>
+            </>
+            <>
+            <input type="number" placeholder="Filter for UserID" value={filter} onChange={(e) => setFilter(e.target.value)}/>
             </>
             <br /><br />
             <>
@@ -61,7 +66,7 @@ export default function App() {
                         <tbody>
                             {data.map((item) => (
                                 <tr key={item.id}>
-                                    <td>{item.userID}</td>
+                                    <td>{item.userId}</td>
                                     <td>{item.id}</td>
                                     <td>{item.title}</td>
                                     <td>{item.body}</td>
